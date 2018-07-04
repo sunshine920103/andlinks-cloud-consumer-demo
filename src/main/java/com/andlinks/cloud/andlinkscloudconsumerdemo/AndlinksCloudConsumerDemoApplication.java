8dd73b3e2a719675745f706a0c44285aa8295ac7
chencong <1085576236@qq.com>
2018-07-04 09:55:21 +0800

    first commit

---
+++ src/main/java/com/andlinks/cloud/andlinkscloudconsumerdemo/AndlinksCloudConsumerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.andlinks.cloud.andlinkscloudconsumerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndlinksCloudConsumerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndlinksCloudConsumerDemoApplication.class, args);
+	}
+}
