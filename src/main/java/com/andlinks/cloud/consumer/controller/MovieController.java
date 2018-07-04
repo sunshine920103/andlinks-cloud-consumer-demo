e339df98e016f45b8102d1b7aeb09d44e8317524
chencong <1085576236@qq.com>
2018-07-04 18:29:38 +0800

    feign接口demo完善

--- src/main/java/com/andlinks/cloud/consumer/controller/MovieController.java
+++ src/main/java/com/andlinks/cloud/consumer/controller/MovieController.java
@@ -3,17 +3,38 @@ package com.andlinks.cloud.consumer.controller;
 import com.andlinks.cloud.consumer.entity.User;
 import com.andlinks.cloud.consumer.feign.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class MovieController {
-  @Autowired
-  private UserFeignClient userFeignClient;
+    @Autowired
+    private UserFeignClient userFeignClient;
 
-  @GetMapping("/user/{id}")
-  public User findById(@PathVariable Long id) {
-    return this.userFeignClient.findById(id);
-  }
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userFeignClient.findById(id);
+    }
+
+    @PostMapping("/getu")
+    public User findUser(@RequestBody User user){
+        return this.userFeignClient.get1(user.getId(),user.getUsername());
+    }
+
+    @GetMapping("/getm")
+    public User findUser2(@RequestParam Long id,@RequestParam String username){
+        Map<String,Object> map=new HashMap<>();
+        map.put("id",id);
+        map.put("username",username);
+        return this.userFeignClient.get2(map);
+    }
+
+    @PostMapping("/postu")
+    public User postUser(@RequestBody User user){
+        return this.userFeignClient.postUser(user);
+    }
+
+
 }
--- src/main/java/com/andlinks/cloud/consumer/feign/UserFeignClient.java
+++ src/main/java/com/andlinks/cloud/consumer/feign/UserFeignClient.java
@@ -2,12 +2,22 @@ package com.andlinks.cloud.consumer.feign;
 
 import com.andlinks.cloud.consumer.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @FeignClient(name = "andlinks-cloud-provider-user")
 public interface UserFeignClient {
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public User findById(@PathVariable("id") Long id);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User findById(@PathVariable("id") Long id);
+
+    @GetMapping("/getUser")
+    public User get1(@RequestParam("id") Long id,@RequestParam("username") String username);
+
+    @GetMapping("getByMap")
+    public User get2(@RequestParam Map<String,Object> m );
+
+    @PostMapping("/postUser")  //主要通过mapping地址来定位对应提供者的fun
+    public User postUser(@RequestBody User user);
+
 }
